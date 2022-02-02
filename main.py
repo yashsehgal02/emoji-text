a70fd790159c13a1c13be6728a8418a6f24b08f6
yashsehgal02 <98725482+yashsehgal02@users.noreply.github.com>
2022-02-02 12:06:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+import os
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand
-import os
 import requests
 
 bot = commands.Bot(command_prefix = "/")
-slash = SlashCommand(bot,sync_commands=True)
 
-@slash.slash(description="with this command you can convert text to emoji")
+@bot.command(name="text2emoji")
 async def text2emoji(ctx, *, text):
     try:
         data = requests.get(f"https://nodejs-beta.yash0221.repl.co/{text}")
-        await ctx.send(data.content)
+        await ctx.send(data.content.decode("utf-8"))
+
     except Exception as e:
         embedVar = discord.Embed(title="💀 error", description=e, color=0xADD8E6)
-        await ctx.channel.send(embed=embedVar)
-    
+        await ctx.send(embed=embedVar)
+       
 bot.run(os.getenv['Token'])
+
+        
